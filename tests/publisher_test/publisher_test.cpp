7a4fb3db5e8269c52adbafef6f4814a069d0f378
smenon <smenon@ethz.ch>
2021-10-11 21:50:36 +0200

    added dependencies, better test

--- tests/publisher_test/publisher_test.cpp
+++ tests/publisher_test/publisher_test.cpp
@@ -1,11 +1,12 @@
 #include "PositionPubSubTypes.h"
 #include "default_publisher.h"
-#include "quadcopter_msgs/msgs/PositionCommand.h"
+#include "geometry_msgs/msgs/Pose.h"
+#include "sensor_msgs/msgs/Mocap.h"
 #include <chrono>
 
 int main() {
-  // <essage
-  msgs::PositionCommand position_cmd;
+  // Message
+  msgs::Pose position;
 
   // Create publisher with msg type
   DDSPublisher position_pub(PositionPubSubType(), "position");
@@ -13,7 +14,7 @@ int main() {
   // Initialize publisher with topic name
   if (position_pub.init()) {
     for (int i = 0; i < 10; i++) {
-      position_pub.publish(position_cmd);
+      position_pub.publish(position);
       std::this_thread::sleep_for(std::chrono::milliseconds(250));
     }
   }
