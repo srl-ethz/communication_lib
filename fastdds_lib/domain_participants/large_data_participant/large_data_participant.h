2d8c0c01ef388b8cee793f84acf5b36a3178de9f
smenon <smenon@ethz.ch>
2021-10-20 20:46:16 +0200

    added large data domain participant

---
+++ fastdds_lib/domain_participants/large_data_participant/large_data_participant.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cstring>
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+
+class LargeDataParticipant {
+public:
+  LargeDataParticipant(int domain_id, std::string qos_name);
+  ~LargeDataParticipant();
+
+private:
+  // Domain id (participants with different domain id's can't communicate)
+  int domain_id_{};
+
+  // QOS name
+  std::string qos_name_{};
+
+  // Pointer to participant
+  eprosima::fastdds::dds::DomainParticipant *participant_;
+
+  // QOS for participant
+  eprosima::fastdds::dds::DomainParticipantQos p_qos;
+
+public:
+  /// Getter function
+  const std::string participant_name() const { return qos_name_; }
+
+  /// Getter function
+  eprosima::fastdds::dds::DomainParticipant *participant() const {
+    return participant_;
+  }
+};
